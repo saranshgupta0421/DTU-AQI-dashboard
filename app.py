6fd2134ef23b2e0cd5e04f9d5cf9725e73608da4
Saransh Gupta <saranshgupta_ene_25b18045@dtu.ac.in>
2026-09-14 17:50:54 +0530

    Refactor app layout and data handling for AQI
    Updated the app layout and data fetching logic to use the new df_long DataFrame. Adjusted the CSV output filename and ensured the layout components reference the correct data.

--- app.py
+++ app.py
@@ -4,48 +4,12 @@ import pandas as pd
 import plotly.express as px
 from dash import Dash, dcc, html, Input, Output
 
-df = pd.read_csv("dtu_data_cleaned.csv ")
-# df = df[["parameter","value","datetimeLocal"]]
-# print(df.head())
-# print(df["parameter"].unique())
+df = pd.read_csv("dtu_data_cleaned.csv")
 df["datetimeLocal"]=pd.to_datetime(df["datetimeLocal"])
 
 
 app = Dash(__name__)
 server = app.server
-
-app.layout = html.Div([
-    html.H1("Delhi AQI"),
-    html.Div(id="last-updated-text"),
-    dcc.Interval(id="refresh-interval",interval=60*60*1000,n_intervals = 0),
-    dcc.Store(id="data-store"),
-    
-    dcc.DatePickerRange(
-        id="date-picker-range",
-        max_date_allowed=df["datetimeLocal"].max().date(),
-        min_date_allowed=df["datetimeLocal"].min().date(),
-        start_date=df["datetimeLocal"].min().date(),
-        end_date=df["datetimeLocal"].max().date(),
-        initial_visible_month=df["datetimeLocal"].min().date()
-    ),
-    dcc.Dropdown(
-        id="pollutant-dropdown",
-        options=[{"label":i.upper(),"value":i} for i in df["parameter"].unique()],
-    
-        value="pm10"),
-    dcc.Graph(id="pollutant-graph")
-])
-r = requests.get(
-    "https://air-quality-api.open-meteo.com/v1/air-quality",
-    params={
-        "latitude": 28.65,
-        "longitude": 77.23,
-        "hourly": "pm2_5,pm10",
-    },
-)
-print(r.status_code)
-print(r.json())
-
 def fetch_live_data():
     r = requests.get(
         "https://air-quality-api.open-meteo.com/v1/air-quality",
@@ -55,12 +19,9 @@ def fetch_live_data():
             "hourly": "pm2_5,pm10",
         },
     )
-
-
     if r.status_code == 200:
         df = pd.DataFrame(r.json()["hourly"])
         df["time"]=pd.to_datetime(df["time"],format="%Y-%m-%dT%H:%M")
-        # df["parameter"] = df.columns[1] 
         df_long = df.melt(
             id_vars="time",
             value_vars=["pm2_5", "pm10"],
@@ -68,13 +29,38 @@ def fetch_live_data():
             value_name="value",
         )
         df_long = df_long.rename(columns={"time": "datetimeLocal"})
-        df_long.to_csv("air_quality_data_long.csv", index=False)
+        df_long.to_csv("air_quality_data_longer.csv", index=False)
+        return df_long
         
         
     else:
         print(f"Error fetching data: {r.status_code}")
-# # df = pd.DataFrame(r.json()["hourly"])
-# df.to_csv("air_quality_data.csv", index=False)
+        return None
+df_long = fetch_live_data()   
+
+app.layout = html.Div([
+    html.H1("Delhi AQI"),
+    html.Div(id="last-updated-text"),
+    dcc.Interval(id="refresh-interval",interval=60*60*1000,n_intervals = 0),
+    dcc.Store(id="data-store"),
+    
+    dcc.DatePickerRange(
+        id="date-picker-range",
+        max_date_allowed=df_long["datetimeLocal"].max().date(),
+        min_date_allowed=df_long["datetimeLocal"].min().date(),
+        start_date=df_long["datetimeLocal"].min().date(),
+        end_date=df_long["datetimeLocal"].max().date(),
+        initial_visible_month=df_long["datetimeLocal"].min().date()
+    ),
+    dcc.Dropdown(
+        id="pollutant-dropdown",
+        options=[{"label":i.upper(),"value":i} for i in df_long["parameter"].unique()],
+    
+        value="pm10"),
+    dcc.Graph(id="pollutant-graph")
+])
+
+
 @app.callback(
     Output("data-store","data"),
     Output("last-updated-text","children"),
@@ -88,7 +74,6 @@ def refresh_data(n):
         Output("pollutant-graph","figure"),
         [Input("pollutant-dropdown","value"), Input("date-picker-range","start_date"), Input("date-picker-range","end_date"),Input("data-store","data")]
         )
-    
 def update_chart(selected_pollutant,start_date,end_date,data):
     if data is None:
         return px.line(title="Loading data...")
